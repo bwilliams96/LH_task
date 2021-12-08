4a37156c0d7d73c6be3f1c3eb86e2506977eb026
Brendan Williams <45268630+bwilliams96@users.noreply.github.com>
2021-12-08 12:49:38 +0000

    Update getOutcome to work with random choices

--- Functions/Work/getOutcome.m
+++ Functions/Work/getOutcome.m
@@ -1,3 +1,7 @@
-function [exp, outcome] = getOutcome(exp, trial, phase)
+function exp = getOutcome(exp, trial, phase)
     exp = updateCount(exp, trial, phase);
-    outcome = exp
+    if exp.selected(trial) == 4
+        exp.outcome(trial) = exp.learn(exp.counts(exp.order(phase,exp.random(trial))),exp.order(phase,exp.random(trial)));
+    else 
+        exp.outcome(trial) = exp.learn(exp.counts(exp.order(phase,exp.selected(trial))),exp.order(phase,exp.selected(trial)));
+    end
--- Functions/Work/updateCount.m
+++ Functions/Work/updateCount.m
@@ -1,5 +1,4 @@
 function exp = updateCount(exp, trial, phase)
-    exp.counts(exp.order(phase,exp.selected(trial))) = exp.counts(exp.order(phase,exp.selected(trial))) + 1;
     if exp.selected(trial) == 4
         exp.random(trial) = randi([1 3],1);
         exp.counts(exp.order(phase,exp.random(trial))) = exp.counts(exp.order(phase,exp.random(trial))) +1;
