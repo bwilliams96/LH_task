1d2d154e4b0985599a839750776c09c666303931
Brendan Williams <45268630+bwilliams96@users.noreply.github.com>
2022-07-18 13:48:47 +0100

    squeeze joystick code initial commit

---
+++ buttonpress.m
@@ -0,0 +1,54 @@
+function [allvec, waitvec, pressvec, rt, rt_intime, squeezetime, force, falsestart, time] = buttonpress()
+
+    clear;
+    clc;
+
+    %% Miscellaneous setup
+    Screen('Preference', 'SkipSyncTests', 1); %%%!!!! ONLY FOR TESTING
+    KbName('UnifyKeyNames');
+    activeKeys = [KbName('b') KbName('y') KbName('g') KbName('r') KbName('t')];
+    RestrictKeysForKbCheck(activeKeys);
+    %medoc = serialport(,9600)
+    medoc = 'medoc';
+    %ListenChar(2);
+    HideCursor;
+
+    %% Now we will setup psychtoolbox
+    % Psychtoolbox setup options from https://peterscarfe.com/insertedCode/TotallyMinimalWithInfoDemo.html
+   
+    % Here we call some default settings for setting up Psychtoolbox
+    PsychDefaultSetup(2);
+    
+    % Get the screen numbers. 
+    screens = Screen('Screens');
+    
+    % To draw we select the maximum of these numbers. So in a situation where we
+    % have two screens attached to our monitor we will draw to the external
+    % screen. If I were to select the minimum of these numbers then I would be
+    % displaying on the physical screen of my laptop.
+    screenNumber = max(screens);
+    
+    % Define black and white (white will be 1 and black 0). This is because
+    % luminace values are genrally defined between 0 and 1.
+    white = WhiteIndex(screenNumber);
+    black = BlackIndex(screenNumber);
+    
+    % Do a simply calculation to calculate the luminance value for grey. This
+    % will be half the luminace value for white
+    grey = white / 2;
+    
+    % Open an on screen window and color it grey. 
+    [window, windowRect] = PsychImaging('OpenWindow', screenNumber, grey);
+    
+    % Get the size of the on screen window in pixels, these are the last two
+    % numbers in "windowRect" 
+    [screenXpixels, screenYpixels] = Screen('WindowSize', window);
+
+    %% RUN THE FUNCTION
+    addpath(genpath('./joystick'));
+    waittime = normrnd(1, 0.005);
+    max_rt = duration(0,0,2);
+    [allvec, waitvec, pressvec, rt, rt_intime, squeezetime, force, falsestart, time] = squeezeptb(window, screenYpixels, waittime, max_rt);
+    %% end of task
+    ListenChar(1);
+    sca;
---
+++ joystick/squeezeptb.m
@@ -0,0 +1,72 @@
+function [allvec, waitvec, pressvec, rt, rt_intime, squeezetime, force, falsestart, time] = squeezeptb(window, screenYpixels, waittime, max_rt)
+
+    waitvec = []; % just values for 'Ready...' period
+    allvec = []; % all values output by squeeze button
+    pressvec = []; % values output when squeezing
+    time = []; % save current time associated with values in allvec
+    
+    tic
+
+    while true
+        % display ready during waittime and record from joystick
+        DrawFormattedText(window, 'Ready...', 'center', 'center',screenYpixels * 0.75, [0 0 1]);
+        Screen('Flip', window);
+        while toc < waittime
+            [~, y, ~, ~] = WinJoystickMex(0);
+            waitvec = [waitvec, y];
+            allvec = [allvec, y];
+            time = [time, datetime(now, 'ConvertFrom','datenum', 'Format', 'HH:mm:ss.SSS')];
+        end
+        
+        % these values are used in the next section for accurate timing etc
+        go_time = time(end);
+        time_length = length(time);
+
+        % this is used as part of the check to see if response was given
+        % within rt
+        gate = false;
+
+        % loop over response time, note, this is 1.5x maximum rt in length
+        DrawFormattedText(window, 'Go!', 'center', 'center',screenYpixels * 0.75, [0 0 1]);
+        Screen('Flip', window);
+        while datetime(now, 'ConvertFrom','datenum', 'Format', 'HH:mm:ss.SSS') < go_time + max_rt*1.5    
+            [~, y, ~, ~] = WinJoystickMex(0);
+            pressvec = [pressvec, y];
+            allvec = [allvec, y];
+            time = [time, datetime(now, 'ConvertFrom','datenum', 'Format', 'HH:mm:ss.SSS')];
+
+        % get length of the time array at maximum rt to determine if
+        % response is given in time
+            if datetime(now, 'ConvertFrom','datenum', 'Format', 'HH:mm:ss.SSS') >= go_time + max_rt
+                if gate == false
+                    rt_time_length = length(time);
+                    gate = true;
+                end
+            end
+        end
+
+        % find where button was pressed and return true if it is within the
+        % maximum rt, otherwise return false
+        change = findchangepts(pressvec,'MaxNumChanges',3,'Statistic',"linear");
+        rt = milliseconds(time(time_length + change(1)) - go_time);
+        if time_length + change(1) <= rt_time_length
+            rt_intime = true;
+        else
+            rt_intime = false;
+        end
+    
+        % determine if a false start was given, note, this is data driven and
+        % based on a point being < 2sd from mean
+        falsestart = findchangepts(waitvec,'MaxNumChanges',3,'Statistic',"linear");
+        if (mean(waitvec) - 2*std(waitvec)) > waitvec(falsestart(2))
+            falsestart = true;
+        else
+            falsestart = false;
+        end
+
+        squeezetime = milliseconds(time(time_length + change(3)) - time(time_length + change(1))); % record amount of time button was squeezed for
+        force = pressvec(change(1)) - pressvec(change(2));
+        break
+    end
+
+% tiledlayout(2,2); nexttile; plot(waitvec); nexttile; plot(pressvec); nexttile; plot(allvec); nexttile; plot(allvec(length(waitvec):end));
