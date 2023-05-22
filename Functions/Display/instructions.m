fd4e33d7df4a2448634d0a56fe47cff0b398001d
Rich Harrison <rich.harrison@reading.ac.uk>
2023-05-22 15:59:41 +0100

    Update instructions function to add space

--- Functions/Display/instructions.m
+++ Functions/Display/instructions.m
@@ -1,6 +1,6 @@
 function instructions(input, window, screenYpixels)
     %Screen('TextSize', window, 70);
     %[screenXpixels, screenYpixels] = Screen('WindowSize', window);
-    DrawFormattedText(window, input, 'center', 'center',[0 0 0], [0 0 1]);
+    DrawFormattedText(window, input, 'center', 'center',[1 1 1], [0 0 1], 0, 0, 3);
     Screen('Flip', window);
-    KbStrokeWait;
+    KbStrokeWait;       
